d9c3dace7aaef5a1840ef0d7289d73c767027981
Ashmeet <120484393+ash-k121@users.noreply.github.com>
2025-07-09 20:05:52 +0530

    Update embed.py

--- app/embedding/embed.py
+++ app/embedding/embed.py
@@ -6,4 +6,4 @@ def create_vector_db(docs):
     embedder = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
     db = FAISS.from_documents(docs, embedder)
     db.save_local("faiss_index")
-    print("✅ FAISS index saved to 'faiss_index/'")
+    print("FAISS index saved to 'faiss_index/'")
